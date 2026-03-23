aaaf451c74be83822efa34b13f37eadeed94eb5b
Enrico Tan III <enrico.sag@gmail.com>
2026-03-23 10:40:43 +0800

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,6 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
-import PasswordGate from './components/PasswordGate';
 
 export default function App() {
-  return (
-    <PasswordGate>
-      <RouterProvider router={router} />
-    </PasswordGate>
-  );
+  return <RouterProvider router={router} />;
 }
